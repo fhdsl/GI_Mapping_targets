b0c53f6c4ac97bffa6df9787341791da6abf8e24
Candace Savonen <cansav09@gmail.com>
2025-02-26 12:56:03 -0500

    Lol logic was right, test was wrong.

--- tests/testthat/test-gi-calc.R
+++ tests/testthat/test-gi-calc.R
@@ -56,14 +56,13 @@ test_that("Test Genetic Interaction score calculations using LFC", {
 })
 
 
-test_that("Test Genetic Interaction score calculations using LFC", {
+test_that("Test Genetic Interaction score calculations by rep", {
   testthat::skip_on_cran()
   gimap_dataset <- get_example_data("gimap") %>%
     gimap_filter() %>%
     gimap_annotate(cell_line = "HELA") %>%
     gimap_normalize(
       timepoints = "day",
-      adj_method = "no_adjustment"
     ) %>%
     calc_gi(stats_by_rep = TRUE)
 })
